81b79f095814ca22196cb851233bc86bb4a37bba
unknown <nandagb23@gmail.com>
2024-01-06 22:09:27 -0300

    adicionando tela de cadastro

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+  
+  imageUrl: string = ''
+
+  signupForm: FormGroup = new FormGroup({});
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      full_name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      password_confirmation: ['', Validators.required],
+    });
+
+    this.imageUrl = 'assets/images/boy-and-dog.png';
+  }
+
+  submitForm() {
+    if(this.signupForm.valid) {
+      console.log(this.signupForm.value);
+    }
+  }
 }
